6bd49f0db8d0b8d7afba35866e63d2620fc7f02b
FoRA <fora22@naver.com>
2020-07-16 18:21:35 +0900

    update

--- Python_Study_to_eon/Number_3.py
+++ Python_Study_to_eon/Number_3.py
@@ -5,62 +5,49 @@ class Solution:
         for i in range(len(voters)):
             self.v.append(list(map(int,voters[i])))
         self.politicianNumber = len(self.v[0])
-        self.victory = [0 for x in range(self.politicianNumber)] # 각 후보자의 득표수를 저장하는 리스트 + 가상의 탈락 후보자 자리를 위해 1을 더한다.
-        for i in range(len(voters) - 1):
-            self.checkVictory()
 
-        return self.v[0]
-        
+        while True:
+            self.victory = [int(self.politicianNumber / 2) for x in range(self.politicianNumber)] # 각 후보자의 득표수를 저장하는 리스트 + 가상의 탈락 후보자 자리를 위해 1을 더한다.
+            polticianVictory = self.checkVictory()
+            if polticianVictory > -1:
+                break
+
+        return polticianVictory
+
+
+    def delMin(self):
+        for politician, victoryNumber in enumerate(self.victory):
+            if victoryNumber == min(self.victory):
+                [x.remove(politician) for x in self.v]
+                break
+            
     def checkVictory(self):
         for i in range(len(self.v)):
-            self.victory[int(self.v[i][0])] = self.victory[int(self.v[i][0])] + 1
+            if self.victory[int(self.v[i][0])] == None:
+                self.victory[int(self.v[i][0])] = 1
+            else:     
+                self.victory[int(self.v[i][0])] = self.victory[int(self.v[i][0])] + 1
+             
 
-        leaving_out_pol = []
-        leaving_out_pol.append(self.victory.index(min(self.victory)))
-        for i in range(len(self.v)):
-            self.v[i].remove(leaving_out_pol[0])
-
-        leaving_out_pol.pop()
-        
-    #     k = 0
-    #     self.checkVictory(k)
-    #     self.victory = [0 for x in range(self.politicianNumber + 1)] # 각 후보자의 득표수를 저장하는 리스트 + 가상의 탈락 후보자 자리를 위해 1을 더한다.
-    #     self.checkVictory(k)
-        
-    #     return self.victory
-
-    def checkVictory(self, k):
-        for i in range(len(self.v)):
-            if int(self.v[i][k]) == self.politicianNumber:
-                k = k + 1
-                self.victory[int(self.v[i][k])] = self.victory[int(self.v[i][k])] + 1
-                k = k - 1
+        if (self.victory.count(max(self.victory)) == 1) and (max(self.victory) > int(len(self.v) / 2)):
+            return self.victory.index(max(self.victory))
+        else:
+            if self.victory.count(min(self.victory)) != 1:
+                for i in range(self.victory.count(min(self.victory))):
+                    self.delMin()
             else:
-                self.victory[int(self.v[i][k])] = self.victory[int(self.v[i][k])] + 1
-
-        if (max(self.victory[:-1]) > (self.politicianNumber / 2)) :
-            return self.victory.index(max(self.victory[:-1]))
-            
-
-        leaving_out_pol = []
-        for i in range(len(self.victory) - 1): # 가상의 후보자 자리는 뺀다.
-            if self.victory[i] == min(self.victory[:-1]):
-                leaving_out_pol.append(i)
-
-        for i in range(len(self.v)):
-            self.v[i] = self.v[i].replace(str(leaving_out_pol[0]), str(self.politicianNumber))
+                self.delMin()
         
-    # def findSameValue(self):
-    #     sameValue = False
-    #     for i in range(len(self.victory[:-1])-1):
+        return -1
+    
 
 
 
 
 
 # voters = ["01","10","01","01","10"]
-voters = ["120","102","210","021","012"]
+# voters = ["120","102","210","021","012"]
 # voters = ["10","01"]
-# voters = ["3120","3012","1032","3120","2031","2103","1230","1230"]
+voters = ["3120","3012","1032","3120","2031","2103","1230","1230"]
 answer = Solution()
 print(answer.solution(voters))
